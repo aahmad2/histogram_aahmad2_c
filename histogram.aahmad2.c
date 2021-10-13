c0a7f0d36ea1739fa587ccf74ff7bbbf64c0015c
alexdowns <alexandra.downs@uvm.edu>
2021-10-13 13:59:01 -0400

    updated from cs office hours

--- histogram.aahmad2.c
+++ histogram.aahmad2.c
@@ -20,27 +20,64 @@ typedef struct {
 int arr [N];
 
 
+// Tallygood function is fine, histogram is working as it should
+// problem is with double r = drand48();
+//arr[i] = r*maxval/2
+// basically not producing a uniform distribution
+// not exceeding 255, but also must be spread out over the span of a million elements
+//
+
+// Added tallySerial, this is used to compare with tallyGood, will need for tallyBad as well
+int tallySerial(int *histogram) {
+    int j;
+    for (j = 0; j < MAXVAL; ++j) {
+        histogram[j] = 0;
+    }
+    for (int i = 0; i < N; ++i) {
+        histogram[arr[i]] += 1;
+    }
+}
 
 
 
 
 
 void tallyGood(void *param){
-    //int hist[N];
-
+    int hist[MAXVAL];
+    pthread_mutex_t *mutex;
+    mutex = (pthread_mutex_t *) param;
     ThreadInfo *T = (ThreadInfo *) param;
-    for (int i=0; i<N; ++i)
-        T->histogram[arr[i]] += 1;
+    int *globHist = T->histogram;
+    // have each thread look between each part it is supposed to look @, then set in the hist arr
+    // at the end, use mutex to stitch together each count from all *T
+    // pthread mutex lock
+
+    for (int i= 0; i < MAXVAL; ++i) {
+        hist[i] = 0;
+    }
+    for (int i=T->startIndex; i<=T->endIndex; ++i)
+        hist[arr[i]] += 1;
+
+    // lock mutex
+
+
+    // iterate through hist
+    pthread_mutex_lock(mutex);
+    for (int j = 0; j < MAXVAL; ++j)
+        globHist[j] += hist[j];
+    pthread_mutex_unlock(mutex);
+
+    pthread_exit(0);
 }
 int main() {
 
     pthread_t tids[N];
 
 
-    //TODO: FIX THIS
+    // JASON SAID THIS WAS FINE, GOT RID OF THE DIVIDED BY I PORTION
     for (int i = 1; i<N ;i++){
         double r = drand48();
-        arr[i] = (r * MAXVAL/i);
+        arr[i] = r * MAXVAL;
     }
 
     pthread_mutex_t mutex;
@@ -72,8 +109,15 @@ int main() {
         histogram[j] = 0;
 
     gettimeofday(&t1, NULL);
-    for (int k=0; k<NUM_THREADS; ++k)
-        pthread_create(&tids[k], NULL, tallyGood, &thread[k]);
+    int newHistogram[MAXVAL];
+
+    tallySerial(newHistogram);
+    pthread_create(&tids[0], NULL, tallyGood, &thread[0]);
+    pthread_create(&tids[1], NULL, tallyGood, &thread[1]);
+    pthread_create(&tids[2], NULL, tallyGood, &thread[2]);
+    pthread_create(&tids[3], NULL, tallyGood, &thread[3]);
+   // for (int k=0; k<NUM_THREADS; ++k)
+   //     pthread_create(&tids[k], NULL, tallyGood, &thread[k]);
 
     for (int k=0; k<NUM_THREADS; ++k)
         pthread_join(tids[k], NULL);
@@ -81,8 +125,14 @@ int main() {
     struct timeval t2;
     gettimeofday(&t2, NULL);
 
+    // for each
+    for (int i = 0; i < MAXVAL; i++) {
+        if (newHistogram[i] != histogram[i]) {
+            printf("no match");
+        }
+    }
 
-    int x;
+    //int x;
     return 0;
 
 
