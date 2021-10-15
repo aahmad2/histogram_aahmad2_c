fd5abff47fa420de2a975587772cb951ac7805b9
Robert Michael Erickson <aahmad2@uvm.edu>
2021-10-14 20:38:15 -0400

    deleted bad comments

--- histogram.aahmad2.c
+++ histogram.aahmad2.c
@@ -7,7 +7,6 @@
 #define NUM_THREADS 4
 #define MAXVAL 256
 
-//Make a histogram, set all the vals to 0.
 int values[MAXVAL];
 int histogram[MAXVAL];
 int histogramBad[MAXVAL];
@@ -18,24 +17,13 @@ typedef struct {
     int *histogram; // the global histogram pthread_mutex_t *mutex; // a mutex, for synchronization
 } ThreadInfo;
 
-int arr [N];
-
-
-// Tallygood function is fine, histogram is working as it should
-// problem is with double r = drand48();
-//arr[i] = r*maxval/2
-// basically not producing a uniform distribution
-// not exceeding 255, but also must be spread out over the span of a million elements
-//
-
-// Added tallySerial, this is used to compare with tallyGood, will need for tallyBad as well
 int tallySerial(int *histogram) {
     int j;
     for (j = 0; j < MAXVAL; ++j) {
         histogram[j] = 0;
     }
     for (int i = 0; i < N; ++i) {
-        histogram[arr[i]] += 1;
+        histogram[values[i]] += 1;
     }
 }
 
@@ -49,20 +37,13 @@ void tallyGood(void *param){
     mutex = (pthread_mutex_t *) param;
     ThreadInfo *T = (ThreadInfo *) param;
     int *globHist = T->histogram;
-    // have each thread look between each part it is supposed to look @, then set in the hist arr
-    // at the end, use mutex to stitch together each count from all *T
-    // pthread mutex lock
 
     for (int i = 0; i < MAXVAL; ++i) {
         hist[i] = 0;
     }
     for (int i=T->startIndex; i<=T->endIndex; ++i)
-        hist[arr[i]] += 1;
+        hist[values[i]] += 1;
 
-    // lock mutex
-
-
-    // iterate through hist
     pthread_mutex_lock(mutex);
     for (int j = 0; j < MAXVAL; ++j)
         globHist[j] += hist[j];
@@ -77,11 +58,10 @@ void *tallyBad(void *param) {
     pthread_mutex_t *mutex;
     mutex = (pthread_mutex_t *) param;
     ThreadInfo *T = (ThreadInfo *) param;
-    // this is the code inside each thread
-    // each thread knows its own start and end values -- myStart and myEnd for (i=myStart; i<=myEnd; ++i) {
+
     for (int i=T->startIndex; i<=T->endIndex; ++i) {
         pthread_mutex_lock(mutex);
-        hist[arr[i]] += 1;
+        hist[values[i]] += 1;
         pthread_mutex_unlock(mutex);
     }
     for (int j = 0; j < MAXVAL; ++j)
@@ -93,10 +73,9 @@ int main() {
     pthread_t tids[N];
 
 
-    // JASON SAID THIS WAS FINE, GOT RID OF THE DIVIDED BY I PORTION
     for (int i = 1; i<N ;i++){
         double r = drand48();
-        arr[i] = r * MAXVAL;
+        values[i] = r * MAXVAL;
     }
 
     pthread_mutex_t mutex;
@@ -174,21 +153,10 @@ int main() {
     for (int k=0; k<NUM_THREADS; ++k)
         pthread_join(tids[k], NULL);
 
-
-
-
-
     struct timeval t2;
     gettimeofday(&t2, NULL);
 
-    // for each
-    for (int i = 0; i < MAXVAL; i++) {
-        if (newHistogram[i] != histogram[i]) {
-            printf("no match");
-        }
-    }
 
-    //int x;
     return 0;
 
 
